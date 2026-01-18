d33cb3114edf72bdbda429aa0ef1a17947792a24
Luke Tan <tanzyluke@gmail.com>
2026-01-18 22:47:30 +0800

    Changed Duke to Cors

---
+++ src/main/java/Cors.java
@@ -0,0 +1,11 @@
+public class Cors {
+    public static void main(String[] args) {
+        String logo = " ██████   ████████  ████████    ████████\n"
+                    + "██       ██      ██ ██      ██ ██\n"
+                    + "██       ██      ██ ████████    ██████\n"
+                    + "██       ██      ██ ██   ██          ██\n"
+                    + " ██████   ████████  ██     ██  ████████\n";
+
+        System.out.println("Hello! I'm your friendly chatbot, Cors!\n" + logo);
+    }
+}
