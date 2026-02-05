67f333cb30052491a2230c3cf25106e27408ef8e
Luke Tan <tanzyluke@gmail.com>
2026-02-05 17:43:25 +0800

    Add JavaDoc to Storage and TaskList

--- src/main/java/cors/task/TaskList.java
+++ src/main/java/cors/task/TaskList.java
@@ -7,19 +7,30 @@ import cors.ui.Ui;
 
 import java.util.ArrayList;
 
+/**
+ * Represents a list of tasks for the chatbot.
+ * Handles operations such as adding, deleting, marking, and retrieving tasks.
+ */
 public class TaskList {
     private ArrayList<Task> myList;
     private int length;
 
+    /**
+     * Constructs an empty TaskList with an initial capacity of 100.
+     */
     public TaskList() {
         this.myList = new ArrayList<>(100);
         this.length = 0;
     }
 
+    /**
+     * Constructs a TaskList by parsing an array of task strings, typically from storage.
+     * @param tasks An array of strings representing serialized tasks from a file.
+     */
     public TaskList(String[] tasks) {
         TaskList taskList = new TaskList();
         Parser parser = new Parser();
-        for (int i = 0; i < 100; i ++) {
+        for (int i = 0; i < 100; i++) {
             Command c = parser.parseFromFile(tasks[i]);
             taskList = c.runCommand(taskList, new Ui(), null);
         }
@@ -27,6 +38,11 @@ public class TaskList {
         this.length = taskList.length;
     }
 
+    /**
+     * Retrieves the string representation of a task at a specific index.
+     * @param index The zero-based index of the task.
+     * @return The string representation of the task, or an empty string if index is invalid.
+     */
     public String get(int index) {
         if (index < length) {
             return this.myList.get(index).toString();
@@ -34,6 +50,10 @@ public class TaskList {
         return "";
     }
 
+    /**
+     * Returns all tasks in the list as an array of strings.
+     * @return A String array containing the string representation of every task.
+     */
     public String[] getAllTasks() {
         String[] allTasks = new String[length];
         for (int i = 0; i < length; i++) {
@@ -42,6 +62,11 @@ public class TaskList {
         return allTasks;
     }
 
+    /**
+     * Marks a task as completed.
+     * @param index The zero-based index of the task to mark.
+     * @return true if the operation was successful, false if the index was out of bounds.
+     */
     public boolean mark(int index) {
         if (index >= length || index < 0) {
             System.out.println("Sorry, there is no task at index " + (index + 1));
@@ -51,6 +76,11 @@ public class TaskList {
         return true;
     }
 
+    /**
+     * Marks a task as incomplete.
+     * @param index The zero-based index of the task to unmark.
+     * @return true if the operation was successful, false if the index was out of bounds.
+     */
     public boolean unmark(int index) {
         if (index >= length || index < 0) {
             System.out.println("Sorry, there is no task at index " + (index + 1));
@@ -61,7 +91,9 @@ public class TaskList {
     }
 
     /**
-     * Adds a new Todo task
+     * Adds a new task to the list and returns a confirmation message.
+     * @param task The task object to be added.
+     * @return A String message confirming the addition and current list size.
      */
     public String add(Task task) {
         myList.add(task);
@@ -70,8 +102,13 @@ public class TaskList {
                 + "\nNow you have " + (length) + " tasks in your list.";
     }
 
+    /**
+     * Deletes a task from the list based on its index.
+     * @param num The zero-based index of the task to be removed.
+     * @throws IncorrectTextException If the index provided is negative or out of bounds.
+     */
     public void delete(int num) {
-        if (num < 0 || num > myList.size()) {
+        if (num < 0 || num >= myList.size()) {
             throw new IncorrectTextException();
         } else {
             System.out.println("Ok, I have removed this task:\n" + myList.get(num) +
